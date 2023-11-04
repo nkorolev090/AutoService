6b5d4f51d12ad90e77721faa54d06eeeca1ac79d
nkorolev090 <nkorolev090@gmail.com>
2023-11-04 14:41:23 +0300

    data

--- AddRegistration.xaml.cs
+++ AddRegistration.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,26 @@ namespace AutoService
     /// </summary>
     public partial class AddRegistration : Window
     {
+        ObservableCollection<gridItem> items;
         public AddRegistration()
         {
+            
             InitializeComponent();
+            items= new ObservableCollection<gridItem>();
+
+            items.Add(new gridItem { time = "10:00", fullName = "Иванов И.И.", cost = 1000 });
+            items.Add(new gridItem { time = "11:00", fullName = "Петров А.И.", cost = 1500 });
+            items.Add(new gridItem { time = "12:30", fullName = "Иванов И.И.", cost = 1000 });
+            itemsGrid.ItemsSource = items;
         }
+
+        
+    }
+
+    public class gridItem
+    {
+        public string time { get; set; }
+        public string fullName { get; set; }
+        public int cost { get; set; }
     }
 }
